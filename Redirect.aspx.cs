d67724bd61b8bf74612ad1a77decbbd0aa6799ef
aamod joshi <aamod.joshi@hotmail.com>
2022-11-18 15:33:01 +0530

    New Commit

---
+++ Redirect.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Redirect : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Redirect("http://103.228.152.227/HMCBikeLeadWebService/hmc_lead_cms/");
+    }
+}
